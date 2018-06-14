3199b80429a64a6c2df0bf2f239c4bb0a572f78e
Jin <jinu0125@gmail.com>
2018-06-13 17:51:37 -0700

    added file

---
+++ app/src/main/java/com/example/laon/myta/GreenFragment.java
@@ -0,0 +1,19 @@
+package com.example.laon.myta;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by laon on 2018-06-13.
+ */
+
+public class GreenFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflacter, ViewGroup container, Bundle savedIncetanceState) {
+        View view = inflacter.inflate(R.layout.fragment_green, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/laon/myta/RedFragment.java
@@ -0,0 +1,19 @@
+package com.example.laon.myta;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by laon on 2018-06-13.
+ */
+
+public class RedFragment extends Fragment
+{
+    public View onCreateView(LayoutInflater inflacter, ViewGroup container, Bundle savedIncetanceState) {
+        View view = inflacter.inflate(R.layout.fragment_red, container, false);
+        return view;
+    }
+}
